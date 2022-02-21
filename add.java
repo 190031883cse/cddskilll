876398f5caa35a993b6edcc8f85176e4211359ef
190031883cse <93446427+190031883cse@users.noreply.github.com>
2022-02-21 08:09:32 +0530

    hai

---
+++ add.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}[A[A[A[A[A[A[A[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[add[B[B[B[B[B[B[B[B[B[B
